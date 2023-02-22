9f9b95bb8b4dae8741711e5146f4ce5cdcf1aafe
Igor Fortti <igormfortti@gmail.com>
2023-02-22 11:33:55 -0300

    criando TabBar

---
+++ appBackFront/Features/Home/HomeVC.swift
@@ -0,0 +1,17 @@
+//
+//  HomeVC.swift
+//  appBackFront
+//
+//  Created by Igor Fortti on 22/02/23.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
