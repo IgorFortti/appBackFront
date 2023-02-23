70c47f8f688b47af3e8ffbe88d947e02a08d6585
Igor Fortti <igormfortti@gmail.com>
2023-02-23 09:35:29 -0300

    Criando logica na viewModel para service e configurando protocols

--- appBackFront/Service/HomeService/HomeService.swift
+++ appBackFront/Service/HomeService/HomeService.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import Alamofire
 
 protocol HomeServiceDelegate: GenericService {
     func getHomeDataFromJson(completion: @escaping completion<NFTData?>)
+    func getHome(completion: @escaping completion<NFTData?>)
 }
 
 class HomeService: HomeServiceDelegate {
+    
+    func getHome(completion: @escaping completion<NFTData?>) {
+        let url: String = "https://run.mocky.io/v3/965fa785-b67f-4631-9b03-2656de1e7bdf"
+        
+        AF.request(url, method: .get).validate(statusCode: 200...299).responseDecodable(of: NFTData.self) { response in
+            debugPrint(response)
+            
+            switch response.result {
+                
+            case .success(let success):
+                print("SUCCESS -> \(#function)")
+                completion(success, nil)
+            case .failure(let error):
+                print("ERROR -> \(#function)")
+                completion(nil, Error.errorRequest(error))
+            }
+        }
+    }
     
     
     
